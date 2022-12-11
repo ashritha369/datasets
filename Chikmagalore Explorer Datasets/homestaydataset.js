5bb3b80843fa98a9644860e5679342ab268b3abf
ashritha369 <ashrithagowda666@gmail.com>
2022-12-11 17:58:27 +0530

    Ckm Xplorer: added temples data

---
+++ Chikmagalore Explorer Datasets/homestaydataset.js
@@ -0,0 +1,81 @@
+export const homestays = [
+  {
+    MainTitle: " Shree Sarva Siddhi Maha Ganapathy Swamy Temple",
+    Place: "Sumsay",
+    SubTitle: "Vinayaka Temple",
+    About: "This temple is located amidst the tea estate in Sumse.",
+    AboutVideo: "https://youtu.be/IgpliDT3j10",
+    HowToReach: "Put image from ckm to this place.",
+    Location:
+      "https://www.google.com/maps/place/Samse+Ganapati+temple/@13.19577,75.3354728,15z/data=!4m5!3m4!1s0x0:0x962a927ebc08c7d1!8m2!3d13.1957698!4d75.3354869",
+    Blog: "https://malnadstays.com/a-sojourn-to-the-flavorful-tea-gardens-of-samse/",
+    TemplePic1: "",
+    TemplePic2: "",
+    TemplePic3: "",
+    SearchQuery: "temples",
+  },
+  {
+    MainTitle: " Sri Deviramma Bettada Temple",
+    Place: "Bindiga",
+    SubTitle: "Deviramma Temple",
+    About:
+      "This temple is located at the base of a hill which is eye-catching!.",
+    AboutVideo: "https://youtu.be/naZKQUYJw2U",
+    HowToReach: "Put image from ckm to this place.",
+    Location:
+      "https://www.google.com/maps/place/Sri+Deviramma+Temple/@13.4178499,75.791567,17z/data=!3m1!4b1!4m5!3m4!1s0x3bbadeb2679a3e69:0xe23a005cececcb6c!8m2!3d13.4178499!4d75.7937557",
+    Blog: "https://utsav.gov.in/view-event/devirammajatre-bindiga-1",
+    TemplePic1: "",
+    TemplePic2: "",
+    TemplePic3: "",
+    SearchQuery: "temples",
+  },
+  {
+    MainTitle: " Sri Sharadamba Temple",
+    Place: "Sringeri",
+    SubTitle: "Sri Sharadamba Temple",
+    About:
+      "The Sharadamba Temple at Sringeri is an 8th-century temple, founded by Sri Adi Shankaracharya, dedicated to goddess Saraswati in the holy town of Sringeri.",
+    AboutVideo: "https://youtu.be/d6wT38ll9S0",
+    HowToReach: "Put image from ckm to this place.",
+    Location:
+      "https://www.google.com/maps/place/Sri+Sharadamba+Temple/@13.4161453,75.2342279,15z/data=!3m1!4b1!4m5!3m4!1s0x3bbb4163302b1b15:0x60508b7cede515a8!8m2!3d13.4161459!4d75.2511368",
+    Blog: "https://www.inditales.com/sringeri-sharada-peetham-sharadamba-temple/",
+    TemplePic1: "",
+    TemplePic2: "",
+    TemplePic3: "",
+    SearchQuery: "temples",
+  },
+  {
+    MainTitle: " Sri Kalabhairaveshwara Swami Gudi",
+    Place: "Devaramane, Mudigere",
+    SubTitle: "Kalabhairava Temple",
+    About:
+      " The temple of Devaramane is dedicated to Kalabhairava. It is a very old temple dating back to 11th century AD.",
+    AboutVideo: "https://youtu.be/M4SooLZb-Cc",
+    HowToReach: "Put image from ckm to this place.",
+    Location:
+      "https://www.google.com/maps/place/Sri+Kalabhairaveshwara+Swami+Gudi/@13.0615687,75.5362177,17z/data=!3m1!4b1!4m5!3m4!1s0x3ba4d20d4f74d673:0xbedbece7539185a9!8m2!3d13.061567!4d75.5384681",
+    Blog: "https://vanamalaarts.org/travelogue/kalabhairava-temple-devaramane-mudigere-chikkamagalur-district-karnataka/",
+    TemplePic1: "",
+    TemplePic2: "",
+    TemplePic3: "",
+    SearchQuery: "temples",
+  },
+  {
+    MainTitle: " Adhishakthyathmaka Sri Annapoorneshwari Ammanavara Gudi",
+    Place: "Horanadu",
+    SubTitle: "Sri Annapoorneshwari Temple.",
+    About:
+      " Nestled in the midst of evergreen hills is Horanadu. It is about 10 kms from Kalasa and has the temple of goddess. Sri Annapoorneshwari.",
+    AboutVideo: "https://youtu.be/piFY7HbMFgw",
+    HowToReach: "Put image from ckm to this place.",
+    Location:
+      "https://www.google.com/maps/place/Adhishakthyathmaka+Sri+Annapoorneshwari+Ammanavara+Gudi/@13.2765336,75.3428066,17z/data=!3m1!4b1!4m5!3m4!1s0x3bbb37e060ccf5b3:0x74031e2d5f52c0ca!8m2!3d13.2765322!4d75.3428182",
+    Blog: "https://chikkamagaluru.nic.in/en/tourist-place/sri-ksetra-horanadu/",
+    TemplePic1: "",
+    TemplePic2: "",
+    TemplePic3: "",
+    SearchQuery: "temples",
+  },
+];
