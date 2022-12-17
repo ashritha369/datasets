6c5a6c7e3b66f1df25ca2328d2ec1e9b733efaab
ashritha369 <ashrithagowda666@gmail.com>
2022-12-17 15:35:28 +0530

    added falls data

---
+++ Chikmagalore Explorer Datasets/fallsdataset.js
@@ -0,0 +1,51 @@
+export const fallsdataset = [
+  {
+    MainTitle: " Hebbe Falls",
+    Place: "Kesavinamane, Chickmagluru",
+    SubTitle: "Hebbe Falls",
+    About:
+      "This is an imposing waterfall in Chikmagalur, which drops from a 551-ft height amidst the coffee plantation in Chikmagalur. Hiking inside the coffee estate will give you the amazing views of the upper waterfall and the lower waterfall. ",
+    AboutVideo: "https://youtu.be/g6mkpOMtR8s",
+    HowToReach: "Put image from ckm to this place.",
+    Location:
+      "https://www.google.com/maps/dir/13.1431081,75.6107956/GPRG%2BJ44+Hebbe+Waterfalls,+Chikkamagaluru+District,+Kesavinamane,+Karnataka+577131/@13.342101,75.3383986,10z/data=!3m1!4b1!4m17!1m6!3m5!1s0x0:0xd42a2f920d592b51!2sHebbe+Waterfalls!8m2!3d13.5415198!4d75.725324!4m9!1m1!4e1!1m5!1m1!1s0x3bbae02df4f9a961:0xd42a2f920d592b51!2m2!1d75.725324!2d13.5415198!3e3",
+    UsefulLinks: "https://stepstogether.in/2021/08/18/hebbe-falls/",
+    FallsPic1: "",
+    FallsPic2: "",
+    FallsPic3: "",
+    SearchQuery: "falls",
+  },
+  {
+    MainTitle: " Jhari Falls",
+    Place: "Inam Dattathreya Peeta",
+    SubTitle: "Hebbe Falls",
+    About:
+      "This is a must visit place in Chikmagalur at around 23 km drive from the city, this is one of the most beautiful and eye catchy waterfalls. Surrounded by dense forest and coffee plantation, the fall is located far inside on the steep and curvy road down the hill, which is nearly 4-5 kms from the vehicle parking area. ",
+    AboutVideo: "https://youtu.be/SX-Vs9EVIoo",
+    HowToReach: "Put image from ckm to this place.",
+    Location:
+      "https://www.google.com/maps/dir/13.1431081,75.6107956/Jhari+water+falls+path,+Inam+Dattathreya+Peeta,+Karnataka+577131/@13.2631265,75.4926674,11z/data=!3m1!4b1!4m18!1m7!3m6!1s0x3bbadf374ddfd279:0x8d07df9b9f2e1612!2sJhari+water+falls+path,+Inam+Dattathreya+Peeta,+Karnataka+577131!3b1!8m2!3d13.4214718!4d75.7423854!4m9!1m1!4e1!1m5!1m1!1s0x3bbadf374ddfd279:0x8d07df9b9f2e1612!2m2!1d75.7423854!2d13.4214718!3e3",
+    UsefulLinks:
+      "https://www.tripadvisor.in/Attraction_Review-g297629-d10423861-Reviews-Jhari_Waterfalls-Chikmagalur_Chikkamagaluru_District_Karnataka.html",
+    FallsPic1: "",
+    FallsPic2: "",
+    FallsPic3: "",
+    SearchQuery: "falls",
+  },
+  {
+    MainTitle: "Kallathigiri Falls",
+    Place: "Kallathigiri, Chikmagalur",
+    SubTitle: "Kallathigiri Falls",
+    About:
+      "The Kallathigiri Falls, also known as the Kalhatti Falls, is a magnificent natural abode located 50 kilometres away from Chikmakagalur. This waterfall is a perfect destination for leisure mongers and thrill seekers alike, and is popular among both locals as well as tourists. The fall gushes down the majestic Chandra Dona Mountains from a height of about 400 feet.",
+    AboutVideo: "https://youtu.be/qrshamCgEeE",
+    HowToReach: "Put image from ckm to this place.",
+    Location:
+      "https://www.google.com/maps/dir//GQPJ%2BGRG,+Chikmagalur,+Kallathgiri,+Karnataka+577129/@13.5363039,75.7120277,12z/data=!4m8!4m7!1m0!1m5!1m1!1s0x3bbae0df48e2b44d:0xbda3e01dcd0c561b!2m2!1d75.7820682!2d13.5363136",
+    UsefulLinks: "https://www.thrillophilia.com/attractions/kallathigiri-falls",
+    FallsPic1: "",
+    FallsPic2: "",
+    FallsPic3: "",
+    SearchQuery: "falls",
+  },
+];
